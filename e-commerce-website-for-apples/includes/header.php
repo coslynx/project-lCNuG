20d1d87624d8ae4e3a5834351853f90e4cac8072
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 12:43:22 +0530

    Added: `e-commerce-website-for-apples/includes/header.php`

---
+++ e-commerce-website-for-apples/includes/header.php
@@ -0,0 +1,23 @@
+// header.php
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce Website for Apples</title>
+    <link rel="stylesheet" href="../public/styles.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="checkout.php">Checkout</a></li>
+            </ul>
+        </nav>
+    </header>
