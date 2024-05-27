e40636b5fa0a960d64c4cbc7052b24fa142cbd81
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 12:43:00 +0530

    Added: `e-commerce-website-for-apples/public/index.php`

---
+++ e-commerce-website-for-apples/public/index.php
@@ -0,0 +1,9 @@
+// e-commerce-website-for-apples/public/index.php
+
+<?php
+
+require_once('../includes/header.php');
+require_once('../pages/home.php');
+require_once('../includes/footer.php');
+
+?>
