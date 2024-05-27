3f4d6fbbd6635a54b95adbd4d66b1b6d62566a46
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 12:43:54 +0530

    Added: `e-commerce-website-for-apples/database/config.php`

---
+++ e-commerce-website-for-apples/database/config.php
@@ -0,0 +1,16 @@
+// config.php
+
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'apple_store');
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
