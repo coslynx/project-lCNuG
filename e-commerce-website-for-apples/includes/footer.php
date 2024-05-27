d2754e8885f94a68c7e4607df79b4261c258ed23
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 12:43:27 +0530

    Added: `e-commerce-website-for-apples/includes/footer.php`

---
+++ e-commerce-website-for-apples/includes/footer.php
@@ -0,0 +1,29 @@
+// footer.php
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Contact Us</h3>
+                <p>Email: info@applestore.com</p>
+                <p>Phone: 123-456-7890</p>
+            </div>
+            <div class="col-md-4">
+                <h3>Follow Us</h3>
+                <ul class="social-media">
+                    <li><a href="https://www.facebook.com/applestore"><img src="assets/icons/facebook-icon.png" alt="Facebook"></a></li>
+                    <li><a href="https://www.twitter.com/applestore"><img src="assets/icons/twitter-icon.png" alt="Twitter"></a></li>
+                    <li><a href="https://www.instagram.com/applestore"><img src="assets/icons/instagram-icon.png" alt="Instagram"></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3>Newsletter</h3>
+                <p>Subscribe to our newsletter for exclusive offers and updates.</p>
+                <form action="subscribe.php" method="post">
+                    <input type="email" name="email" placeholder="Enter your email" required>
+                    <button type="submit">Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</footer>
